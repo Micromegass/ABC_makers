4b65d644382945f0e15d22991b2ec4bb972437f2
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-03-28 16:32:29 -0500

    created authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
+
   root 'products#index'
 end
