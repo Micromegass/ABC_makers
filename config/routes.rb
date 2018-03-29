d5a0a5600c96715c09bea16a4e91cfddc9eced43
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-03-29 11:18:42 -0500

    added the ability to comment on each product

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    resources :comments, only: [:create]
+  end
 
   root 'products#index'
 end
