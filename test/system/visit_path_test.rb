d37cf07b8c632517e00c47a1d6d2230313203990
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-04-26 13:13:47 -0500

    Delete tests

--- test/system/visit_path_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class VisitPathTest < ApplicationSystemTestCase
-    test "user can visit page" do
-        visit root_path
-        sleep(5)
-        assert_equal root_path, current_path
-    end
-end
